2a95b0bc8c08f2cfbd81e57fe824def3dc919ec7
klinzmandr <dave.klinzman@yahoo.com>
2017-09-18 11:10:58 -0700

    initial creation of the index file updater.

---
+++ updater.php
@@ -0,0 +1,15 @@
+<?php
+
+/* USAGE
+** This file is to be used to update the index.php file in each directory of the
+** repository.  This is necessary because the index.php file is copied into each 
+** directory as it is added by the repository manager.  So, any change to the 
+** index.php file itself requires that the new file be propagated into each existing   
+** directory of the repository.
+
+** Usually, this file will reside in the db sub-folder and copied into the root folder 
+** only when it is to be used.  It should then be deleted so it is not inadvertently 
+** used.
+*/
+
+?>
